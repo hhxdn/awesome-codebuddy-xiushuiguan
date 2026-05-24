020a1466d5813aa7ba6a1abcffa09453fd446935
ascendking <ascendking@hhxdn.com>
2026-05-24 18:18:54 +0800

    chore: 后端端口改为6001，管理后台端口改为16001

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -3,10 +3,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 3000,
+    port: 16001,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8080',
+        target: 'http://127.0.0.1:6001',
         changeOrigin: true,
         pathRewrite: {
           '^/api': '/api'
